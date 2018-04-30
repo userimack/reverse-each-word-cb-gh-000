52a69c98f9ec2a1bced6dac2ead8c4f86bec2f35
Mahendra Yadav <mahendra.k12@gmail.com>
2018-04-30 16:00:06 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word(sentence)
+  # new_sentence = Array.new 
+  # sentence.split.each { |word| new_sentence.push(word.reverse)}
+  sentence = sentence.split.collect { |word| word.reverse}
+  sentence.join(" ")
+end 
+
